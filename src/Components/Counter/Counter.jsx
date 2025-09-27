3ff0130e5744d4ff065fcdd1d973a90a0b1bea81
CoderElham <mdelham.official@gmail.com>
2025-09-28 00:44:53 +0600

    fixed issues in counter

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -4,15 +4,14 @@ const Counter = ({ data }) => {
 
   const inProgressData = data.filter(element => element.status == "In-Progress")
   const resolvedData = data.filter(element => element.status === "Resolved");
-  console.log(inProgressData);
 
   return (
     <div className='w-11/12 mx-auto mt-20 flex flex-col md:flex-row gap-5'>
 
       <div className='w-full relative'>
         <div className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] flex justify-between items-center rounded-2xl'>
-          <div><img src="/public/vector1.png" alt="" /></div>
-          <div><img src="/public/vector1.png" alt="" className='scale-x-[-1]' /></div>
+          <div><img src="./vector1.png" alt="vector" /></div>
+          <div><img src="./vector1.png" alt="vector" className='scale-x-[-1]' /></div>
         </div>
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
           <p className='text-[20px]'>In Progress</p>
@@ -22,8 +21,8 @@ const Counter = ({ data }) => {
 
       <div className='w-full relative'>
         <div className='bg-gradient-to-r from-[#54CF68] to-[#00827A] flex items-center justify-between rounded-2xl'>
-          <div><img src="/public/vector1.png" alt="" /></div>
-          <div><img src="/public/vector1.png" alt="" className='scale-x-[-1]' /></div>
+          <div><img src="./vector1.png" alt="vector" /></div>
+          <div><img src="./vector1.png" alt="vector" className='scale-x-[-1]' /></div>
         </div>
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
           <p className='text-[20px]'>Resolved</p>
